7ed0ea954a046018472a6cb802c6bcbe05889f81
measty <20169086+measty@users.noreply.github.com>
2022-07-05 15:54:51 +0100

    dont kill on session end

--- tiatoolbox/visualization/render_demo/app_hooks.py
+++ tiatoolbox/visualization/render_demo/app_hooks.py
@@ -1,5 +1,8 @@
 import sys
+import requests
 
 def on_session_destroyed(session_context):
     # If present, this function executes when the server closes a session.
-    sys.exit()
+    fname = r'/tiatoolbox/app_data/slides/TCGA-SC-A6LN-01Z-00-DX1.svs'
+    fname = fname='-*-'.join(fname.split('/'))
+    resp = requests.get(f'http://127.0.0.1:5000/changeslide/slide/{fname}')
