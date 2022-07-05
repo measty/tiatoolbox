a7d37de2952c58d09fdbe74cb654ca483de55250
measty <20169086+measty@users.noreply.github.com>
2022-07-05 16:24:06 +0100

    config updates

--- tiatoolbox/visualization/render_demo/app_hooks.py
+++ tiatoolbox/visualization/render_demo/app_hooks.py
@@ -1,8 +1,10 @@
 import sys
+
 import requests
 
+
 def on_session_destroyed(session_context):
-    # If present, this function executes when the server closes a session.
-    fname = r'/tiatoolbox/app_data/slides/TCGA-SC-A6LN-01Z-00-DX1.svs'
-    fname = fname='-*-'.join(fname.split('/'))
-    resp = requests.get(f'http://127.0.0.1:5000/changeslide/slide/{fname}')
+    # If present, this function executes when the server closes session.
+    fname = r"/tiatoolbox/app_data/slides/TCGA-SC-A6LN-01Z-00-DX1.svs"
+    fname = fname = "-*-".join(fname.split("/"))
+    resp = requests.get(f"http://127.0.0.1:5000/changeslide/slide/{fname}")
