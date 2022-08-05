fix .dat scaling on load

tiatoolbox/visualization/tileserver.py
@@ -434,7 +434,7 @@ class TileServer(Flask):
             SQ = SQLiteStore.from_geojson(overlay_path)
         elif overlay_path.suffix == ".dat":
             SQ = SQLiteStore(auto_commit=False)
-            SQ.from_dat(overlay_path, 1 / np.array(self.slide_mpp))
+            SQ.from_dat(overlay_path, 1)#1 / np.array(self.slide_mpp))
         elif overlay_path.suffix in [".jpg", ".png", ".tiff"]:
             layer = f"layer{len(self.tia_pyramids)}"
             if overlay_path.suffix == ".tiff":