f4022a619b6192c37b38671d63879904bcbd5a59
measty <20169086+measty@users.noreply.github.com>
2023-07-19 15:07:13 +0100

    server settings

--- tiatoolbox/visualization/bokeh_app/main.py
+++ tiatoolbox/visualization/bokeh_app/main.py
@@ -2037,7 +2037,7 @@ if curdoc().session_context is not None:
     do_doc = True
 print(f"req args are: {req_args}")
 
-is_deployed = False
+is_deployed = True
 rand_id = token.generate_session_id()
 print(f"rand id is: {rand_id}")
 first_z = [1]
